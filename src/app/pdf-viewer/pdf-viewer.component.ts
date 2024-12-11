ef91ba321a44bce9e88a8c7856656b47db5e1343
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2024-12-11 16:03:59 -0300

    Lector Pdf: turn.js funcional

---
+++ src/app/pdf-viewer/pdf-viewer.component.ts
@@ -0,0 +1,46 @@
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
+declare var $: any;
+import * as Hammer from 'hammerjs';
+
+@Component({
+  selector: 'app-pdf-viewer',
+  templateUrl: './pdf-viewer.component.html',
+  styleUrls: ['./pdf-viewer.component.scss'],
+  standalone: true
+})
+export class PdfViewerComponent  implements AfterViewInit {
+
+  constructor(private elRef: ElementRef) { }
+
+  ngAfterViewInit() {
+    $('#flipbook').turn({
+      width: 800,
+      height: 600,
+      autoCenter: true,
+      elevation: 50,
+      gradients: true,
+      duration: 1000
+    });
+
+    const flipbookElement = this.elRef.nativeElement.querySelector('#flipbook');
+    const hammer = new Hammer(flipbookElement);
+
+    hammer.on('swipeleft', () => {
+      this.nextPage();
+    });
+
+    hammer.on('swiperight', () => {
+      this.previousPage();
+    });
+  }
+
+  
+  previousPage() {
+    $('#flipbook').turn('previous');
+  }
+
+  nextPage() {
+    $('#flipbook').turn('next');
+  }
+
+}
